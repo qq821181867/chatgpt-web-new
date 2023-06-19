5818f012462fe5d4e93aab4abb84f3f6b271af7c
yisen.Li <821181867@qq.com>
2023-06-19 14:24:23 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig((env) => {
     plugins: setupPlugins(viteEnv),
     server: {
       host: '154.40.59.105',//'0.0.0.0'
-      port: 3006,//3002
+      port: 3002,
       open: false,
       proxy: {
         '/api': {
